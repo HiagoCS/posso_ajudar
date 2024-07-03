a9424c23a2643c76b7951e78a0da176f92899f74
HiagoCS <hiago8cs@gmail.com>
2024-07-03 12:12:52 -0300

    add item_dish_tb migration

---
+++ backend/database/migrations/2024_07_03_151047_item_dish_tb.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_dish_tb', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_ktc_menu');
+            $table->integer('id_ktc_stuff');
+            $table->string('item_qnt_used');
+            $table->foreign('id_ktc_menu')->references('id')->on('ktc_menu');
+            $table->foreign('id_ktc_stuff')->references('id')->on('ktc_stuff');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_dish_tb');
+    }
+};
