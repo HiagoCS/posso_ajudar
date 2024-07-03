b6b5c94b91f2d53fc98523a763c564529cfe9045
HiagoCS <hiago8cs@gmail.com>
2024-07-03 12:05:06 -0300

    add ktc_stuff migration

---
+++ backend/database/migrations/2024_07_03_150325_ktc_stuff.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ktc_stuff', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_nm');
+            $table->string('item_un');
+            $table->string('item_qnt');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ktc_stuff');
+    }
+};
