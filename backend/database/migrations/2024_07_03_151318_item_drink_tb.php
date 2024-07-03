d809ea6234350b005e0a7a1fc4018203a0b97cd9
HiagoCS <hiago8cs@gmail.com>
2024-07-03 12:14:37 -0300

    add item_drink_tb migration

---
+++ backend/database/migrations/2024_07_03_151318_item_drink_tb.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_drink_tb', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_bar_menu');
+            $table->integer('id_bar_stuff');
+            $table->string('item_qnt_used');
+            $table->foreign('id_bar_menu')->references('id')->on('bar_menu');
+            $table->foreign('id_bar_stuff')->references('id')->on('bar_stuff');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_drink_tb');
+    }
+};
