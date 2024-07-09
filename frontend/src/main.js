0c366edad1d544072833d15440ae7db469333152
HiagoCS <hiago8cs@gmail.com>
2024-07-09 17:08:05 -0300

    icons modified

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import * as Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCog, faCamera, faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faCog, faCamera, faAngleDoubleLeft, faEye, faBox, faFile, faCalendar, faTableCellsLarge, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
@@ -21,7 +21,7 @@ const i18n = createI18n({
 })
 
 
-library.add(faCog, faCamera, faAngleDoubleLeft);
+library.add(faCog, faCamera, faAngleDoubleLeft, faEye, faBox, faFile, faCalendar, faTableCellsLarge, faUsers);
 
 const app = Vue.createApp(App).use(i18n).component("font-awesome-icon", FontAwesomeIcon).use(VueSweetalert2)
 
