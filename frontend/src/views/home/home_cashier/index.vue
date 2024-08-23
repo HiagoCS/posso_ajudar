9ada38ee67424d86327e5b2da2ab2063934690d7
HiagoCS <hiago8cs@gmail.com>
2024-08-22 22:16:16 -0300

    act btns for cashier-table

--- frontend/src/views/home/home_cashier/index.vue
+++ frontend/src/views/home/home_cashier/index.vue
@@ -7,7 +7,45 @@
         <div class="d-flex flex-row col-12">
           <!--SELETOR DE MESA-->
         </div>
-        <div class="d-flex flex-row col-12">
+        <div class="d-flex flex-column align-items-center">
+          <div class="d-flex flex-row cashier-table">
+            <div class="d-flex col-12 flex-row">
+              <span class="d-flex justify-content-center col-1 act-btn">
+                <font-awesome-icon icon="filter"/>
+              </span>
+              <span class="d-flex justify-content-center col-1 act-btn">
+                <font-awesome-icon icon="ban"/>
+              </span>
+              <span class="d-flex col-5 act-btn">
+                <font-awesome-icon icon="magnifying-glass"/>
+                <a style="font-size: 70%;">Código de Barra</a>
+              </span>
+              <span class="d-flex col-5 act-btn">
+                <font-awesome-icon icon="magnifying-glass"/>
+                <a style="font-size: 70%;">Produto</a>
+              </span>
+            </div>
+          </div>
+          <div class="d-flex flex-column cashier-table table">
+            <div class="d-flex flex-row justify-content-between table-header">
+              <span>#</span>
+              <span>Cód. Reduzido</span>
+              <span>Cód. Barra</span>
+              <span>Produto</span>
+              <span>Descrição</span>
+              <span>R$</span>
+              <span>Quant.</span>
+            </div>
+            <div class="d-flex flex-row justify-content-between table-items">
+              <span>#</span>
+              <span>Cód. Reduzido</span>
+              <span>Cód. Barra</span>
+              <span>Produto</span>
+              <span>Descrição</span>
+              <span>R$</span>
+              <span>Quant.</span>
+            </div>
+          </div>
           
         </div>
       </div>
