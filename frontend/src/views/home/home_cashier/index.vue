e1475b751adfb104fe6d2c2ed11e100e5c4345a0
HiagoCS <hiago8cs@gmail.com>
2024-08-25 18:45:54 -0300

    cashier-table template

--- frontend/src/views/home/home_cashier/index.vue
+++ frontend/src/views/home/home_cashier/index.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="d-flex flex-column align-items-center">
           <div class="d-flex flex-row cashier-table">
-            <div class="d-flex col-12 flex-row">
+            <div class="d-flex col-12 flex-row act">
               <span class="d-flex justify-content-center col-1 act-btn">
                 <font-awesome-icon icon="filter"/>
               </span>
@@ -26,24 +26,32 @@
               </span>
             </div>
           </div>
-          <div class="d-flex flex-column cashier-table table">
-            <div class="d-flex flex-row justify-content-between table-header">
-              <span>#</span>
-              <span>Cód. Reduzido</span>
-              <span>Cód. Barra</span>
-              <span>Produto</span>
-              <span>Descrição</span>
-              <span>R$</span>
-              <span>Quant.</span>
-            </div>
-            <div class="d-flex flex-row justify-content-between table-items">
-              <span>#</span>
-              <span>Cód. Reduzido</span>
-              <span>Cód. Barra</span>
-              <span>Produto</span>
-              <span>Descrição</span>
-              <span>R$</span>
-              <span>Quant.</span>
+          <div class="d-flex flex-row cashier-table product-table-container">
+            <div class="table-wrapper">
+              <table class="product-table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Cód. Reduzido</th>
+                  <th>Cód. Barra</th>
+                  <th>Produto</th>
+                  <th>Descrição</th>
+                  <th>R$</th>
+                  <th>Quant.</th>
+                </tr>
+              </thead>
+              <tbody>
+              <tr v-for="(product, index) in products" :key="index">
+                <td>{{ index + 1 }}</td>
+                <td>{{ product.codReduzido }}</td>
+                <td>{{ product.codBarra }}</td>
+                <td>{{ product.nome }}</td>
+                <td>{{ product.descricao }}</td>
+                <td>{{ product.preco }}</td>
+                <td>{{ product.quantidade }}</td>
+              </tr>
+            </tbody>
+            </table>
             </div>
           </div>
           
@@ -51,7 +59,15 @@
       </div>
     </main>
   </template>
-
+<!-- <tr>
+  <td>#</td>
+  <td>Cód. Reduzido</td>
+  <td>Cód. Barra</td>
+  <td>Produto</td>
+  <td>Descrição</td>
+  <td>R$</td>
+  <td>Quant.</td>
+</tr> -->
 <script>
 import axios from 'axios'
 import VueCookie from 'vue-cookie';
@@ -63,7 +79,18 @@ export default{
     return{
         user:{},
         roles:[],
-        active:""
+        active:"",
+        count: 30,
+        products: [
+        { codReduzido: '001', codBarra: '123456789', nome: 'Produto 1', descricao: 'Descrição 1', preco: '10.00', quantidade: 1 },
+        { codReduzido: '002', codBarra: '987654321', nome: 'Produto 2', descricao: 'Descrição 2', preco: '15.00', quantidade: 2 },
+        { codReduzido: '001', codBarra: '123456789', nome: 'Produto 1', descricao: 'Descrição 1', preco: '10.00', quantidade: 1 },
+        { codReduzido: '002', codBarra: '987654321', nome: 'Produto 2', descricao: 'Descrição 2', preco: '15.00', quantidade: 2 },
+        { codReduzido: '001', codBarra: '123456789', nome: 'Produto 1', descricao: 'Descrição 1', preco: '10.00', quantidade: 1 },
+        { codReduzido: '002', codBarra: '987654321', nome: 'Produto 2', descricao: 'Descrição 2', preco: '15.00', quantidade: 2 },
+        { codReduzido: '001', codBarra: '123456789', nome: 'Produto 1', descricao: 'Descrição 1', preco: '10.00', quantidade: 1 },
+        { codReduzido: '002', codBarra: '987654321', nome: 'Produto 2', descricao: 'Descrição 2', preco: '15.00', quantidade: 2 }
+      ]
     }
   },
   async created(){
